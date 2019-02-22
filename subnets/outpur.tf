bf82c61db5cd2046e7eb8be936ea4d9139271f41
david <david@shoplineapp.com>
2019-02-22 13:13:33 +0800

    feat(subent): add app and public subents

---
+++ subnets/outpur.tf
@@ -0,0 +1,15 @@
+output "subnet_app_a_id" {
+  value = "${aws_subnet.app-a.id}"
+}
+
+output "subnet_app_b_id" {
+  value = "${aws_subnet.app-b.id}"
+}
+
+output "subent_public_a_id" {
+  value = "${aws_subnet.public-a.id}"
+}
+
+output "subent_public_b_id" {
+  value = "${aws_subnet.public-b.id}"
+}
