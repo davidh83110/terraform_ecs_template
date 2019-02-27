22377e1a4ab7895dc037afcdf8dea033ad79c32d
david <david@shoplineapp.com>
2019-02-27 15:05:02 +0800

    fix(subnets): output naming error

--- subnets/output.tf
+++ subnets/output.tf
@@ -6,10 +6,10 @@ output "subnet_app_b_id" {
   value = "${aws_subnet.app-b.id}"
 }
 
-output "subent_public_a_id" {
+output "subnet_public_a_id" {
   value = "${aws_subnet.public-a.id}"
 }
 
-output "subent_public_b_id" {
+output "subnet_public_b_id" {
   value = "${aws_subnet.public-b.id}"
 }
