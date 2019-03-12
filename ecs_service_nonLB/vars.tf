469f58978ad7cc2c06fb1005e988001c5495d0c5
david <david@shoplineapp.com>
2019-03-12 10:52:32 +0800

    feat(ecs_nonALB): remove useless variables

--- ecs_service_nonLB/vars.tf
+++ ecs_service_nonLB/vars.tf
@@ -4,8 +4,6 @@ variable "ecs_cluster_id" {}
 
 variable "ecs_task_definition" {}
 
-variable "alb_container_port" {}
-
 variable "ecs_desired_count" {}
 
 variable "ecs_minimum_healthy_percent" {}
@@ -17,7 +15,5 @@ variable "ecs_health_check_grace_period_seconds" {}
 variable "ecs_placement_strategy_type" {}
 
 variable "ecs_placement_strategy_field" {}
-
-variable "alb_target_group_id" {}
 
 variable "ecs_service_role" {}
