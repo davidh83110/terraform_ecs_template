73b9237e702aa9d2293866c7cf6465041f53b77b
david <david@shoplineapp.com>
2019-03-13 14:25:25 +0800

    feat(ecs-nonALB): remove iam_role cause no needed

--- ecs_service_nonLB/service.tf
+++ ecs_service_nonLB/service.tf
@@ -2,7 +2,6 @@ resource "aws_ecs_service" "main" {
   name                               = "${var.ecs_service_name}"
   cluster                            = "${var.ecs_cluster_id}"
   desired_count                      = "${var.ecs_desired_count}"
-  iam_role                           = "${var.ecs_service_role}"
   task_definition                    = "${var.ecs_task_definition}"
   deployment_minimum_healthy_percent = "${var.ecs_minimum_healthy_percent}"
   deployment_maximum_percent         = "${var.ecs_maximum_healthy_percent}"
